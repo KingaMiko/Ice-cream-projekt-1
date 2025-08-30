84b1d6b418fb3f8bdfa7448e7b291290fdbd89d6
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2025-08-30 08:49:36 +0200

    modal menu fix

--- src/modal-menu.js
+++ src/modal-menu.js
@@ -1,14 +1,21 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-menu-open]"),
-      closeModalBtn: document.querySelector("[data-modal-menu-close]"),
-      modal: document.querySelector("[data-modal-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-menu-open]'),
+    closeModalBtn: document.querySelector('[data-modal-menu-close]'),
+    modal: document.querySelector('[data-modal-menu]'),
+    menuLinks: document.querySelectorAll('.modal-menu__link'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.add('is-hidden');
+    });
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
